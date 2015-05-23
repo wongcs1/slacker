3a6d3d91f772d2f64bf91831c92cb4d8406be5df
AlexanderMcNeill <mcnear1@student.op.ac.nz>
2015-05-23 15:11:40 +1200

    Finished the first attempt at the channel service. Still need to tidy up abit

--- channel_service/channel_service.py
+++ channel_service/channel_service.py
@@ -9,50 +9,54 @@ class ChannelWebService:
     def __init__(self):
         client = MongoClient()
         self.channel_db = client["channel_db"]
+        self.channel_collection = self.channel_db.channels
 
     @cherrypy.tools.json_out()
     def GET(self, channel_id):
         channel_id = int(channel_id)
-        try:
-            channel_json = self.get_channel_json(channel_id)
-            return {"channel_read_response":
-                        {
-                            "response_code": 0,
-                        },
-                    "channel_data": channel_json
-                    }
-        except IndexError:
-            return {"channel_read_response":
-                        {
-                            "response_code": 1,
-                            "error": "No such channel"
-                        },
-                    }
+        response_root = {"channel_read_response": {"response_message": '', "response_code": 1}}
+        channel_read_response = response_root["channel_read_response"]
 
+        try:
+            channel_json = self.channel_collection.find_one({"_id": channel_id})
+            if channel_json is None:
+                channel_read_response["response_message"] = "No matching channel"
+            else:
+                channel_read_response["response_code"] = 0
+                channel_read_response["response_message"] = "Channel found"
+                response_root["channel"] = channel_json
+        except:
+            channel_read_response["response_message"] = "Unable to connect to database"
 
     @cherrypy.tools.json_out()
     @cherrypy.tools.json_in()
     def POST(self):
+
+        response_root = {"new_channel_response": {"response_message": '', "response_code": 1}}
+        new_channel_response = response_root["new_channel_response"]
+
         try:
-            new_channel_json = cherrypy.request.json
-            valid = self.check_valid_input(new_channel_json)
+            new_channel_request = cherrypy.request.json
 
-            already_exists = self.check_channel_exists(new_channel_json)
+            channel_json = self.channel_collection.find_one({"name": new_channel_request["name"]})
+
+            if channel_json is not None:
+                new_channel_response["response_message"] = "There is already a channel by that name"
+            else:
+                latest_channel_json = self.channel_collection.find_one({"$query":{},"$orderby":{"_id":-1}})
+                new_channel_id = 0
+                if latest_channel_json is not None:
+                    new_channel_id = latest_channel_json["_id"] + 1
+
+                new_channel_json = {"_id": new_channel_id, "name": new_channel_request["name"], "owner": new_channel_request["owner"]}
+
+                self.channel_collection.insert_one(new_channel_json)
+                new_channel_response["response_code"] = 0
+                new_channel_response["response_message"] = "Channel added"
+        except KeyError:
+            new_channel_response["response_message"] = "Invalid channel request"
         except:
-            #respond to if it fails by the input not being valid or the channel already exising
-            pass
-
-    def check_valid_input(self, channel_json):
-        return False
-
-    def get_channel_json(self, channel_id):
-        channel = self.channel_db.channels.find_one({"_id": channel_id})
-
-        if channel is None:
-            raise IndexError
-        else:
-            return channel
-
+            new_channel_response["response_message"] = "Unable to connect to database"
 
 if __name__ == '__main__':
     conf = {
@@ -62,5 +66,5 @@ if __name__ == '__main__':
             "tools.response_headers.headers": [("Content-Type", "application/json")]
             }
         }
-    cherrypy.config.update({'server.socket_port' : 8000})
+    cherrypy.config.update({'server.socket_port': 8000})
     cherrypy.quickstart(ChannelWebService(), '/', conf)
--- channel_service/channel_service_test.py
+++ channel_service/channel_service_test.py
@@ -12,16 +12,24 @@ class ChannelServiceTests(unittest.TestCase):
 
         #Variable's for testing the get channel
         self.valid_channel_id = 1
-        self.invalid_channel_id = 43453
+        self.existing_channel_response = {"channel_read_response": {"response_message": 'Channel found', "response_code": 0}}
 
-        self.valid_new_channel_json = {}
-        self.invalid_new_channel_json = {}
+        self.invalid_channel_id = -1
+        self.no_channel_response = {}
 
+        self.valid_new_channel_json = {"name": "Alex's test", "owner": 1}
+        self.valid_new_channel_response = {}
+
+        self.invalid_new_channel_json = {"nme": "Lex"}
+        self.invalid_new_channel_response = {}
 
 
     def test_get_channel(self):
         get_channel_response = requests.get('http://localhost:8000/?channel_id=' + str(self.valid_channel_id)).json()
+        self.assertDictEqual(self.existing_channel_response, get_channel_response)
 
+        get_channel_response = requests.get('http://localhost:8000/?channel_id=' + str(self.invalid_channel_id)).json()
+        self.assertDictEqual(self.no_channel_response, get_channel_response)
 
     def test_add_channel(self):
         add_channel_response = requests.post('http://localhost:8000/', headers=self.heads,
